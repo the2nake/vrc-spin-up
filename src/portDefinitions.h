f419916eecb550c1e0f671e3945c085e81407de9
the2nake <home.vn2007@gmail.com>
2023-02-04 10:46:05 +0800

    Changed port configuration

--- src/portDefinitions.h
+++ src/portDefinitions.h
@@ -2,9 +2,9 @@
 
 #define INTAKE_PORT 1
 
-#define DRIVE_FL_PORT 15
-#define DRIVE_FR_PORT 16
-#define DRIVE_BR_PORT 17
-#define DRIVE_BL_PORT 18
+#define DRIVE_FL_PORT 14
+#define DRIVE_FR_PORT 15
+#define DRIVE_BR_PORT 16
+#define DRIVE_BL_PORT 17
 
 #define IMU_PORT 19
