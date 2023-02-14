79044bb9b8b7c1d038c9e0da518b4f4c6dce546e
the2nake <home.vn2007@gmail.com>
2023-02-14 12:43:01 +0800

    Added new motor ports

--- src/portDefinitions.h
+++ src/portDefinitions.h
@@ -2,9 +2,14 @@
 
 #define INTAKE_PORT 1
 
+#define FLYWHEEL_PORT_A 11
+#define FLYWHEEL_PORT_R 12
+
 #define DRIVE_FL_PORT 14
 #define DRIVE_FR_PORT 15
 #define DRIVE_BR_PORT 16
 #define DRIVE_BL_PORT 17
 
 #define IMU_PORT 19
+
+#define INDEXER_PORT 20
