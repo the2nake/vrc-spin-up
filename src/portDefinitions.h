571f4023bb59f3923db3d16cf2f74bc2aed4554d
the2nake <home.vn2007@gmail.com>
2023-04-06 08:46:08 +0800

    Modified port locations

--- src/portDefinitions.h
+++ src/portDefinitions.h
@@ -7,17 +7,17 @@
 
 #pragma once
 
-#define FLYWHEEL_PORT 13
+#define FLYWHEEL_PORT 7
 
-#define DRIVE_FL_PORT 14
-#define DRIVE_FR_PORT 15
-#define DRIVE_MR_PORT 16
-#define DRIVE_BR_PORT 17
-#define DRIVE_BL_PORT 18
-#define DRIVE_ML_PORT 19
+#define DRIVE_FL_PORT 1
+#define DRIVE_FR_PORT 2
+#define DRIVE_MR_PORT 3
+#define DRIVE_BR_PORT 4
+#define DRIVE_BL_PORT 5
+#define DRIVE_ML_PORT 6
 
 #define PTO_PORT 'G'
 
-#define IMU_PORT 20
+#define IMU_PORT 12
 
 #define INDEXER_PORT 21
